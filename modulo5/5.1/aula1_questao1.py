dc6dd0f309360bacebc90e30883435ca207acc5e
JulianaElen <163027587+JulianaElen@users.noreply.github.com>
2024-05-16 18:19:02 -0300

    Delete modulo5/5.1 directory

--- modulo5/5.1/aula1_questao1.py
+++
@@ -1,7 +0,0 @@
-# Calcular a diferença absolta entre dois números
-primeiroNumero = float(input("Digite o primeiro número: "))
-segundoNumero = float(input("Digite o segundo número: "))
-
-resultado = abs(primeiroNumero-segundoNumero)
-
-print(f"A diferença absoluta entre os números é: {round(resultado,2)}")
--- modulo5/5.1/aula1_questao4.py
+++
@@ -1,7 +0,0 @@
-# Mostra a data e hora atuais
-
-from datetime import  datetime
-
-data = datetime.now()
-
-print(data.strftime("Data: %d/%m/%Y \nHora: %H:%M"))
